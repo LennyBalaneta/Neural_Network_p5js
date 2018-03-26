74a4852a85d3788c61854ec08661960bf573b277
Marcos Balatka <m.balatka5@gmail.com>
2018-03-26 07:06:54 -0300

    Matrix lib

---
+++ matrix.js
@@ -0,0 +1,68 @@
+class Matrix {
+  constructor(rows, cols) {
+    this.rows = rows;
+    this.cols = cols;
+    this.matrix = [];
+    for(let i=0 ; i<this.rows ; i++) {
+      this.matrix[i] = [];
+      for(let j=0 ; j<this.cols ; j++) {
+        this.matrix[i][j] = 0;
+      }
+    }
+  }
+  
+  randomize() {
+    for(let i=0 ; i<this.rows ; i++) {
+      for(let j=0 ; j<this.cols ; j++) {
+        this.matrix[i][j] = Math.floor(Math.random() * 10);
+      }
+    }
+  }
+  
+  
+  multiply(n) {
+    if(n instanceof Matrix) {
+      //Matrix product
+      if(this.cols !== n.rows) {
+        console.log("this.cols !== n.rows");
+        return undefined;
+      }
+      let a = this;
+      let b = n;
+      let result = new Matrix(a.rows, b.cols);
+      for(let i=0 ; i<result.rows ; i++) {
+        for(let j=0 ; j<result.cols ; j++) {
+          //Dot product of values in col
+          let sum = 0;
+          for(let k=0 ; k<a.cols ; k++) {
+            sum += a.matrix[i][k] * b.matrix[k][j];
+          }
+          result.matrix[i][j] = sum;
+        }
+      }
+      return result;
+    }else {
+      for(let i=0 ; i<this.rows ; i++) {
+        for(let j=0 ; j<this.cols ; j++) {
+          this.matrix[i][j] *= n;
+        }
+      }
+    }
+  }
+  
+  add(n) {
+    if(n instanceof Matrix) {
+      for(let i=0 ; i<this.rows ; i++) {
+        for(let j=0 ; j<this.cols ; j++) {
+          this.matrix[i][j] += n.matrix[i][j];
+        }
+      }
+    }else {
+      for(let i=0 ; i<this.rows ; i++) {
+        for(let j=0 ; j<this.cols ; j++) {
+          this.matrix[i][j] += n;
+        }
+      }
+    }
+  }
+}
